54b85b1ad561e8bb06d3f826ca8b80947b96cc94
moadeyi <76417578+moadeyi@users.noreply.github.com>
2020-12-20 22:41:25 -0500

    Update Power.py
    Added different efficiency and area for Endurosat top panel

--- Power.py
+++ Power.py
@@ -1,5 +1,5 @@
 """
-Power v2.2 with sun
+Power v3.1 with sun
 
 Calculates power based on satellite position with respect to sun
 """
@@ -12,21 +12,24 @@ def flux(long, sunAngle, i0, j0, k0):
     Output: power (Watt)
     k0 points upwards from top of satellite (solar panel exists there)"""
     
-    efficiency = 0.307 * 0.88 * (1 - (75-28) * 0.0022) #efficiency of solar panel
-    Area2U = 0.01076664    # Area of one 2U panel in m^2
+    efficiency2 = 0.307 * 0.88 * (1 - (75-28) * 0.0022) #efficiency of Pumpkin solar panel
+    efficiency1 = 0.295 * 0.88 * (1 - (75-28) * 0.0022)
+    
+    Area2U = 0.01076664    # Area of one 2U Pumpkin panel in m^2
+    Area1U = 0.00603       # Area of one 1U Endurosat panel in m^2
     # using arrays for inertial i and k unit vectors
     
     phi = 1373*(math.cos(23)*np.array([1,0,0]) - math.sin(23)*np.array([0,0,1])) 
     # solar flux vector
     
-    powerTop = phi.dot(k0)*Area2U/2
+    powerTop = phi.dot(k0)*Area1U
     if powerTop < 0:   # top is sunlit
-        powerTop = efficiency*abs(powerTop)
+        powerTop = efficiency1*abs(powerTop)
     else:
         powerTop = 0
 
-    powerSidei0 = efficiency*abs(phi.dot(i0)*Area2U)  # don't double count
-    powerSidej0 = efficiency*abs(phi.dot(j0)*Area2U)*0.75    #reduced to 1U on a side
+    powerSidei0 = efficiency2*abs(phi.dot(i0)*Area2U)  # don't double count
+    powerSidej0 = efficiency2*abs(phi.dot(j0)*Area2U)*0.75    #reduced to 1U on a side
     
     powerAll = powerTop + powerSidei0 + powerSidej0    # before considering dark side
     
